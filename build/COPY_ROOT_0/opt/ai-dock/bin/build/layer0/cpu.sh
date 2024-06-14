2e35bb12bc79ab05c33299622d481a5148963cc5
Rob Ballantyne <rob@dynamedia.uk>
2024-06-14 18:41:40 +0100

    fix function name

--- build/COPY_ROOT_0/opt/ai-dock/bin/build/layer0/cpu.sh
+++ build/COPY_ROOT_0/opt/ai-dock/bin/build/layer0/cpu.sh
@@ -5,7 +5,7 @@ build_cpu_main() {
     build_common_run_tests
 }
 
-build_cpu_install_deps() {
+build_cpu_install_torch() {
     "$PYTHON_VENV_PIP" install --no-cache-dir \
         torch==${PYTORCH_VERSION} \
         torchvision \
